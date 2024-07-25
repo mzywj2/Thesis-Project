bf92c3639bb2777c58a9e4ebc00349344fba1ddc
mzywj2 <151543153+mzywj2@users.noreply.github.com>
2024-07-25 21:08:46 +0100

    Update Run command for N50.sh

--- 1. C.Officinalis HiFi Asm Comparison/Initial Compleasm/N50/Run command for N50.sh
+++ 1. C.Officinalis HiFi Asm Comparison/Initial Compleasm/N50/Run command for N50.sh
@@ -1,2 +1,2 @@
-# To run the N50 script on the selected genome fasta
-./N50.sh 
+# To run the N50 script on the selected genome fasta - For example the hap2 file
+./N50.sh COFF001.asm.bp.hap2.p_ctg.gfa.fa
